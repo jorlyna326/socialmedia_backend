e532313c0fdcdd2846c13bfeab8daf222fc7e89a
Jorlyna Pujols <jorlynapujols@Jorlynas-MBP.attlocal.net>
2023-11-26 14:51:24 -0500

    started user controllers and routes

---
+++ controllers/userController.js
@@ -0,0 +1,75 @@
+const { User } = require("../models");
+
+module.exports = {
+  // /api/users
+  // GET all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // GET a single user by its _id and populated thought and friend data
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select('-__v').populate('thoughts').populate('friends');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // POST a new user:
+  // // example data
+  // {
+  //   "username": "lernantino",
+  //   "email": "lernantino@gmail.com"
+  // }
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // PUT to update a user by its _id
+  async updateUser(req, res) {
+    try {
+        const user = await User.findOneAndUpdate(
+          { _id: req.params.userId },
+          { $set: req.body },
+          { runValidators: true, new: true }
+        );
+  
+        if (!user) {
+          return res.status(404).json({ message: 'No User with this id!' });
+        }
+  
+        res.json(user);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  },
+  // DELETE to remove user by its _id
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      res.json({ message: 'User deleted!' })
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // /api/users/:userId/friends/:friendId
+  // POST to add a new friend to a user's friend list
+
+  // DELETE to remove a friend from a user's friend list
+};
